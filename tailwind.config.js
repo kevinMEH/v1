668418c0be892098fa460290889648c11d91222a
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-17 11:54:04 -0500

    Source Code Pro as default mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+                mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono]
 			},
 		},
 	},
