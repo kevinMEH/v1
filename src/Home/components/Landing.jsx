9e2e01219c67ea358c83fb4f2079254ffcf7617a
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-18 14:13:52 -0500

    Landing: Added support for md and above(?)

--- src/Home/components/Landing.jsx
+++ src/Home/components/Landing.jsx
@@ -2,17 +2,31 @@ import React from 'react';
 
 const Landing = () => {
 	return (
-        <div className="flex-col w-full my-32">
-            <h1 className="text-2xl font-semibold tracking-wider text-black">
-                Hello! 👋
-            </h1>
-            <h1 className="text-4xl font-extrabold tracking-wide leading-relaxed gradientText rotateColor">I'm Kevin,</h1> 
-            <p className="text-gray-500">
-                a web and software developer based in Brooklyn. I
-                specialize in creating unique and memorable web experiences.
-            </p>
-            <img className="my-5 shadow-xl shadow-black/5 rounded-xl h-48 w-full" src="https://images.unsplash.com/photo-1519681393784-d120267933ba?w=1740&q=80" alt="Landing" /> 
-            {/* TODO: Replace placeholder with actual image */}
+        <div className="w-full
+        md:flex md:justify-between md:space-x-12 xl:space-x-28 2xl:space-x-40">
+            <div className="md:flex-1
+            md:my-auto">
+                <h1 className="font-semibold tracking-wider text-black
+                text-2xl md:text-3xl lg:text-4xl">
+                    Hello! 👋
+                </h1>
+                <h1 className="font-extrabold tracking-wide leading-relaxed gradientText rotateColor
+                text-4xl md:text-5xl lg:text-6xl
+                md:leading-relaxed lg:leading-relaxed">
+                    I'm Kevin,
+                </h1> 
+                <p className="text-gray-500
+                sm:text-lg lg:text-xl">
+                    a web and software developer based in Brooklyn. I
+                    specialize in creating unique and memorable web experiences.
+                </p>
+            </div>
+            <div className="md:my-auto md:flex-1">
+                <img className="my-5 md:my-0 shadow-xl shadow-black/5 object-cover w-full
+                rounded-xl md:rounded-2xl lg:rounded-3xl
+                " src="https://images.unsplash.com/photo-1519681393784-d120267933ba?w=1740&q=80" alt="Landing" /> 
+                {/* TODO: Replace placeholder with actual image */}
+            </div>
         </div>
 	);
 };
