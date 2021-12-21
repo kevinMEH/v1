a6c4817d23b0b0d265749295d0aa8a65455932a7
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-21 11:51:43 -0500

    About page redesign

--- src/Home/components/About.jsx
+++ src/Home/components/About.jsx
@@ -4,42 +4,19 @@ import Title from "./subcomponents/Title";
 
 const About = () => {
     return (
-        <div className="flex-col w-full space-y-4 text-gray-500 text-sm text-justify">
-            <Title text="About Me" color="text-blue-500" />
-            <p>
-                Hello! I'm Kevin Liao, a software
-                engineer from Brooklyn. Lorem ipsum dolor sit amet, consectetur 
-                adipiscing elit.
-            </p>
-            
-            <div className="rounded-xl text-left overflow-hidden bg-white shadow-xl shadow-black/5">
-                <div className="flex-none border-b border-blue-500/10">
-                    <div className="flex items-center h-7 space-x-2 pl-3.5">
-                        <div className="w-3 h-3 bg-red-400 rounded-full hover:bg-red-500 cursor-pointer"></div>
-                        <div className="w-3 h-3 bg-yellow-400 rounded-full hover:bg-yellow-500 cursor-pointer"></div>
-                        <div className="w-3 h-3 bg-green-400 rounded-full hover:bg-green-500 cursor-pointer"></div>
-                    </div>
-                </div>
-                <div className="p-5 font-mono">
-                    <div className="">
-                        <h2 className="font-semibold text-lg text-blue-500 mb-4 text-center">
-                            Development Tools
-                        </h2>
-                    </div>
-                    <div className="space-y-4">
-                        <List title="Frontend Development" items={
-                            ["JavaScript", "TypeScript", "React", "TailwindCSS"]
-                        } />
-                        <div className="border-b !mt-0"></div>
-                        <List title="Backend Development" items={
-                            ["NodeJS", "Java", "Rust"]
-                        } />
-                        <div className="border-b !mt-0"></div>
-                        <List title="Other Tools" items={
-                            ["Adobe Photoshop", "Adobe After Effects", "Figma, Framer, and other UI/UX design tools"]
-                        } />
-                    </div>
-                </div>
+        <div className="w-full space-y-4 text-gray-500 text-sm text-justify
+        md:flex md:flex-row-reverse md:space-x-reverse md:space-x-12">
+            <div className="md:flex-1 space-y-4 text-left">
+                <Title text="About Me" color="text-blue-500" />
+                <p>
+                    Hello! I'm Kevin Liao, a software
+                    engineer from Brooklyn. Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit.
+                </p>
+                <h2 className="text-base">Here are some of the technologies I work with:</h2>
+                <List title="Frontend:" items={["JavaScript", "TypeScript", "React", "Tailwind"]} />
+                <List title="Backend:" items={["NodeJS", "Java", "Rust"]} />
+                <List title="Design:" items={["Photoshop", "After Effects", "Framer", "Other UI/UX design tools"]} />
             </div>
         </div>
     );
@@ -52,14 +29,12 @@ const List = ( { title, items } ) => {
     }
     
     return (
-        <>
-            <h3 className="font-medium text-base ml-3">{title}</h3>
-            <div className="pb-4">
-                <ul className="ml-10 list-disc">
-                    {listItems}
-                </ul>
-            </div>
-        </>
+        <div className="text-base space-y-2">
+            <h3 className="font-semibold">{title}</h3>
+            <ul className="ml-10 list-disc font-medium">
+                {listItems}
+            </ul>
+        </div>
     )
 }
 
