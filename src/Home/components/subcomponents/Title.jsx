2828aa87671656187a67d8c6f19e51be6303644a
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-21 17:04:35 -0500

    Title size changes

--- src/Home/components/subcomponents/Title.jsx
+++ src/Home/components/subcomponents/Title.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Title = ({text, color}) => {
     return (
-        <h1 className={"text-2xl font-bold tracking-wide text-center " + color}>
+        <h1 className={"font-bold tracking-wide text-center " + color +
+        " text-2xl sm:text-3xl lg:text-4xl"}>
             {text}
         </h1>
     );
