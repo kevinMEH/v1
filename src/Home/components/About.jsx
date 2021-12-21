76f4e1d8a8f4aeb230043a3eaea7618edee6bceb
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-21 15:30:58 -0500

    Medium screen size support

--- src/Home/components/About.jsx
+++ src/Home/components/About.jsx
@@ -4,7 +4,8 @@ import Title from "./subcomponents/Title";
 
 const About = () => {
     return (
-        <div className="w-full space-y-4 text-gray-500 text-sm text-justify
+        <div className="w-full space-y-4 text-gray-500 text-justify
+        text-sm md:text-base
         md:flex md:flex-row-reverse md:space-x-reverse md:space-x-12">
             <div className="md:flex-1 space-y-4 text-left">
                 <Title text="About Me" color="text-blue-500" />
@@ -13,10 +14,12 @@ const About = () => {
                     engineer from Brooklyn. Lorem ipsum dolor sit amet, consectetur 
                     adipiscing elit.
                 </p>
-                <h2 className="text-base">Here are some of the technologies I work with:</h2>
-                <List title="Frontend:" items={["JavaScript", "TypeScript", "React", "Tailwind"]} />
-                <List title="Backend:" items={["NodeJS", "Java", "Rust"]} />
-                <List title="Design:" items={["Photoshop", "After Effects", "Framer", "Other UI/UX design tools"]} />
+                <h2 className="text-base md:text-lg">Here are some of the technologies I work with:</h2>
+                <div className="md:flex md:justify-between md:mx-12">
+                    <List title="Frontend:" items={["JavaScript", "TypeScript", "React", "Tailwind"]} />
+                    <List title="Backend:" items={["NodeJS", "Java", "Rust"]} />
+                    <List title="Design:" items={["Photoshop", "After Effects", "Framer", "UI/UX tools"]} />
+                </div>
             </div>
         </div>
     );
@@ -29,9 +32,9 @@ const List = ( { title, items } ) => {
     }
     
     return (
-        <div className="text-base space-y-2">
+        <div className="text-base space-y-2 md:flex-1 md:text-lg">
             <h3 className="font-semibold">{title}</h3>
-            <ul className="ml-10 list-disc font-medium">
+            <ul className="ml-10 md:ml-5 list-disc font-medium">
                 {listItems}
             </ul>
         </div>
