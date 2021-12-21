51db1bef7592070ef025b869aefc449ff43595cd
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-21 15:31:23 -0500

    Landing changes

--- src/Home/components/Landing.jsx
+++ src/Home/components/Landing.jsx
@@ -4,8 +4,8 @@ const Landing = () => {
 	return (
         <div className="w-full
         md:flex md:justify-between md:space-x-12 xl:space-x-28 2xl:space-x-40">
-            <div className="md:flex-1
-            md:my-auto">
+
+            <div className="md:flex-1 md:my-auto">
                 <h1 className="font-semibold tracking-wider text-black
                 text-2xl md:text-3xl lg:text-4xl">
                     Hello! 👋
@@ -21,12 +21,14 @@ const Landing = () => {
                     specialize in creating unique and memorable web experiences.
                 </p>
             </div>
+
             <div className="md:my-auto md:flex-1">
                 <img className="my-5 md:my-0 shadow-xl shadow-black/5 object-cover w-full
                 rounded-xl md:rounded-2xl lg:rounded-3xl
                 " src="https://images.unsplash.com/photo-1519681393784-d120267933ba?w=1740&q=80" alt="Landing" /> 
                 {/* TODO: Replace placeholder with actual image */}
             </div>
+            
         </div>
 	);
 };
