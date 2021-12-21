236d83615b95ee8ed1b49169271dcb2a4581a9ed
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-21 17:04:20 -0500

    Portfolio md

--- src/Home/components/Portfolio.jsx
+++ src/Home/components/Portfolio.jsx
@@ -4,7 +4,7 @@ import Title from "./subcomponents/Title";
 
 const Portfolio = () => {
     return (
-        <div className="flex-col w-full space-y-4">
+        <div className="flex-col w-full space-y-4 md:space-y-12">
             <Title text="Selected Works" color="text-teal-500" />
             <Card title="Portfolio" desc="Check out the source code for this portfolio!"
             // TODO: replace placeholder
@@ -21,7 +21,7 @@ const Card = ({title, desc, image, url, tags}) => {
     for(let tag of tags) {
         tagSpans.push(
             <span
-                className="rounded-lg py-1 px-2.5 m-0.5 bg-gray-50 text-xs"
+                className="rounded-lg py-1 px-2.5 m-0.5 bg-gray-50 text-xs md:text-sm"
             >
                 {tag}
             </span>
@@ -29,16 +29,16 @@ const Card = ({title, desc, image, url, tags}) => {
     }
     
     return (
-        <div className="rounded-xl overflow-hidden text-gray-500 bg-white shadow-xl shadow-black/5">
+        <div className="rounded-xl md:rounded-3xl overflow-hidden text-gray-500 bg-white shadow-xl shadow-black/5 md:w-3/4 md:mx-auto">
             <a href={url} >
-                <img className="h-48 w-full"
+                <img className="object-cover h-full"
                     src={image} />
             </a>
             <div className="p-5 space-y-2 rounded-b-xl">
                 <a href={url}>
-                    <h3 className="font-semibold font-mono text-lg text-teal-500">{title}</h3>
+                    <h3 className="font-semibold font-mono text-lg md:text-xl text-teal-500">{title}</h3>
                 </a>
-                <p>{desc}</p>
+                <p className="md:text-lg">{desc}</p>
                 <div className="pt-2 flex flex-wrap space-x-2"> {tagSpans} </div>
             </div>
         </div>
