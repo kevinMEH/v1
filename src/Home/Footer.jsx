6da0a8062cda09b7f8fd273d89034348af52c5ba
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-22 12:51:54 -0500

    Footer added support for larger screen sizes

--- src/Home/Footer.jsx
+++ src/Home/Footer.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="flex justify-between text-sm gradientText rotateColor">
+        <div className="flex justify-between text-sm md:text-base lg:text-lg gradientText rotateColor">
             <div>
                 <p>© 2021 kevinMEH</p>
             </div>
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center space-x-4 md:space-x-14 lg:space-x-24">
                 <a href="https://github.com/kevinMEH">GitHub</a>
                 <a href="mailto:hello@liao.gg">Contact</a>
-                <a href="https://github.com/kevinMEH/portfolio"></a>
             </div>
         </div>
     );
