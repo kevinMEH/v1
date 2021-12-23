07d02676f01b657cef0d74180abd61302d3e88ac
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-22 19:48:51 -0500

    About lg support

--- src/Home/components/About.jsx
+++ src/Home/components/About.jsx
@@ -4,18 +4,18 @@ import Title from "./subcomponents/Title";
 
 const About = () => {
     return (
-        <div className="w-full space-y-4 text-gray-500 text-justify
-        text-sm md:text-base
+        <div className="w-full text-gray-500 text-justify
+        text-sm md:text-base lg:text-lg
         md:flex md:flex-row-reverse md:space-x-reverse md:space-x-12">
-            <div className="md:flex-1 space-y-4 text-left">
+            <div className="md:flex-1 space-y-4 lg:space-y-8 text-left">
                 <Title text="About Me" color="text-blue-500" />
-                <p className="md:py-4">
+                <p className="md:pt-4">
                     Hello! I'm Kevin Liao, a software
                     engineer from Brooklyn. Lorem ipsum dolor sit amet, consectetur 
                     adipiscing elit.
                 </p>
-                <h2 className="text-base md:text-lg">Here are some of the technologies I work with:</h2>
-                <div className="space-y-3 md:space-y-0 md:flex md:justify-around md:mx-8">
+                <h2 className="text-base md:text-lg lg:text-xl">Here are some of the technologies I work with:</h2>
+                <div className="space-y-3 pl-6 md:pl-0 md:space-y-0 md:flex md:justify-around">
                     <List title="Frontend:" items={["JavaScript", "TypeScript", "React", "Tailwind"]} />
                     <List title="Backend:" items={["NodeJS", "Java", "Rust"]} />
                     <List title="Design:" items={["Photoshop", "After Effects", "Framer", "UI/UX tools"]} />
@@ -32,10 +32,12 @@ const List = ( { title, items } ) => {
     }
     
     return (
-        <div className="text-base space-y-2 md:flex-1 md:text-lg lg:flex lg:justify-center">
-            <div>
-                <h3 className="font-semibold">{title}</h3>
-                <ul className="ml-10 md:ml-5 list-disc font-medium space-y-1 md:space-y-0">
+        <div className="text-base
+        md:flex-initial md:text-lg lg:text-xl
+        lg:flex lg:justify-center">
+            <div className="space-y-2">
+                <h3 className="font-semibold lg:pb-1">{title}</h3>
+                <ul className="ml-10 md:ml-5 list-disc space-y-1 md:space-y-0 lg:space-y-1.5">
                     {listItems}
                 </ul>
             </div>
